1edd33b2210568b14a3a611a36201df660b72059
Shinra88 <chokdingo@hotmail.com>
2025-08-03 16:43:28 +0200

    add envTest

--- tests/setup.js
+++ tests/setup.js
@@ -1,17 +1,6 @@
-// tests/setup.js
-console.log('🧪 Configuration de test chargée');
+require('dotenv').config({ path: '.env.test' }); // Charger les variables d'environnement depuis .env.test
 
-// Variables d'environnement pour les tests
-process.env.NODE_ENV = 'test';
-process.env.PORT = '5001';
-process.env.FRONTEND_URL = 'http://localhost:3000';
-process.env.JWT_SECRET = 'test-secret-key-for-testing-only';
-process.env.MYSQL_HOST = 'localhost';
-process.env.MYSQL_USER = 'test';
-process.env.MYSQL_ROOT_PASSWORD = 'test';
-process.env.MYSQL_DATABASE = 'test';
-process.env.DATABASE_URL = 'mysql://test:test@localhost:3306/test';
-process.env.MONGO_URI = 'mongodb://localhost:27017/test';
+console.log('🧪 Configuration de test chargée');
 
 // Configuration des timeouts
 jest.setTimeout(10000);
